65e02b64f541bab251cff3e0f3aa26a04c2f979b
fanzhikang <852672780@qq.com>
2018-10-19 14:38:40 +0800

    添加单选、多选题的爬取

--- demo.py
+++ demo.py
@@ -34,6 +34,7 @@ class Crawl:
         self.list2 = []
         self.list3 = []
         self.list4 = []
+        self.list5 = []
         self.detail_url = 'https://admin.kaoshixing.com/admin/load_data?&id='
         self.session = requests.session()
 
@@ -93,9 +94,41 @@ class Crawl:
                 dict['dif'] = dif
                 self.list3.append(dict)
             elif li['type'] == '多选':
-                pass
+                dict = {}
+                question = li['content']
+                dif = li['difficult']
+                classification = li['classification'].split('/')[-1]
+                id = li['id']
+                url = self.detail_url + id
+                choices, answer, analysis = self.parse_4(url)
+                dict['question'] = question
+                dict['choice1'] = choices[0]
+                dict['choice2'] = choices[1]
+                dict['choice3'] = choices[2]
+                dict['choice4'] = choices[3]
+                dict['answer'] = answer
+                dict['analysis'] = analysis
+                dict['classification'] = classification
+                dict['dif'] = dif
+                self.list4.append(dict)
             elif li['type'] == '单选':
-                pass
+                dict = {}
+                question = li['content']
+                dif = li['difficult']
+                classification = li['classification'].split('/')[-1]
+                id = li['id']
+                url = self.detail_url + id
+                choices, answer, analysis = self.parse_4(url)
+                dict['question'] = question
+                dict['choice1'] = choices[0]
+                dict['choice2'] = choices[1]
+                dict['choice3'] = choices[2]
+                dict['choice4'] = choices[3]
+                dict['answer'] = answer
+                dict['analysis'] = analysis
+                dict['classification'] = classification
+                dict['dif'] = dif
+                self.list5.append(dict)
 
 
 
@@ -127,15 +160,32 @@ class Crawl:
             answer = '错误'
         return answer
 
+    def parse_4(self, url):
+        choices = []
+        data_content = self.session.get(url)
+        data_content = data_content.content.decode(encoding='utf-8')
+        data_dict = json.loads(data_content)
+        choices.append(data_dict['bizContent'].get('answer1', ''))
+        choices.append(data_dict['bizContent'].get('answer2', ''))
+        choices.append(data_dict['bizContent'].get('answer3', ''))
+        choices.append(data_dict['bizContent'].get('answer4', ''))
+        answer = data_dict['bizContent'].get('test_ans_right', '')
+        analysis = data_dict['bizContent'].get('analysis', '')
+        return choices, answer, analysis
+
 
     def save_sheet(self):
         workbook = xlwt.Workbook(encoding='utf-8')
         sheet1 = workbook.add_sheet('问答')
         sheet2 = workbook.add_sheet('填空')
         sheet3 = workbook.add_sheet('判断')
+        sheet4 = workbook.add_sheet('多选')
+        sheet5 = workbook.add_sheet('单选')
         head1 = ['题干', '答案', '分类', '难度']
         head2 = ['题干', '答案1', '答案2', '答案3', '答案4', '分类', '难度']
         head3 = ['题干', '答案', '分类', '难度']
+        head4 = ['题干', '选项A', '选项B', '选项C', '选项D', '答案', '解析', '分类', '难度']
+        head5 = ['题干', '选项A', '选项B', '选项C', '选项D', '答案', '解析', '分类', '难度']
 
         for h in range(len(head1)):
             sheet1.write(0, h, head1[h])
@@ -169,6 +219,37 @@ class Crawl:
             sheet3.write(i, 2, li['classification'])
             sheet3.write(i, 3, li['dif'])
             i += 1
+
+        for h in range(len(head4)):
+            sheet4.write(0, h, head4[h])
+        i = 1
+        for li in demo.list4:
+            sheet4.write(i, 0, li['question'])
+            sheet4.write(i, 1, li['choice1'])
+            sheet4.write(i, 2, li['choice2'])
+            sheet4.write(i, 3, li['choice3'])
+            sheet4.write(i, 4, li['choice4'])
+            sheet4.write(i, 5, li['answer'])
+            sheet4.write(i, 6, li['analysis'])
+            sheet4.write(i, 7, li['classification'])
+            sheet4.write(i, 8, li['dif'])
+            i += 1
+
+        for h in range(len(head5)):
+            sheet5.write(0, h, head5[h])
+        i = 1
+        for li in demo.list5:
+            sheet5.write(i, 0, li['question'])
+            sheet5.write(i, 1, li['choice1'])
+            sheet5.write(i, 2, li['choice2'])
+            sheet5.write(i, 3, li['choice3'])
+            sheet5.write(i, 4, li['choice4'])
+            sheet5.write(i, 5, li['answer'])
+            sheet5.write(i, 6, li['analysis'])
+            sheet5.write(i, 7, li['classification'])
+            sheet5.write(i, 8, li['dif'])
+            i += 1
+
         workbook.save('demo.xls')
 
 
